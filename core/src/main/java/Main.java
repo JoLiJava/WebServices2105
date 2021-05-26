5ee5f4dbb44bd90c3299186f8c1a595121e5c997
JoLiJava <73590210+JoLiJava@users.noreply.github.com>
2021-05-26 07:49:10 +0200

    BufferedReader InputStreamReader

--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
     public static void main(String[] args) {
+        // localhost - 127.0.0.1
+        // 178.174.162.51
 
-        var utils = new Utils();
+        try (ServerSocket socket = new ServerSocket(5050)) {
+            Socket client = socket.accept();
+            System.out.println(client.getInetAddress());
 
-        System.out.println(utils.message());
+            var inputFromClient = new BufferedReader(new InputStreamReader((client.getInputStream())));
+
+            System.out.println(inputFromClient.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
