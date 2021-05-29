9334d6a3ac1b8b931e7c5532a3ce24f844b7c501
JoLiJava <73590210+JoLiJava@users.noreply.github.com>
2021-05-29 12:16:26 +0200

    Sending 404-message from core/server to client. Client can read the message. Cannot read 404 in Firefox. Can read 404 with Edge. Working!

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,16 +1,32 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {                                                   // KLIENT
 
-    public static void main(String[] args) {    // 178.174.162.51 | localhost - 127.0.0.1
-        try {
-            Socket socket = new Socket("localhost", 5050);      // växel
+    public static void main(String[] args) {    // 178.174.162.51 |
+        try {                                   // localhost - 127.0.0.1
+            Socket clientSocket = new Socket("localhost", 5050);      // växel
 
-            var output = new PrintWriter(socket.getOutputStream());
-            output.println("Hello from client");
+            var output = new PrintWriter(clientSocket.getOutputStream());
+            output.println("Hello from client\r\n\r\n");
             output.flush();
+
+            var inputFromServer
+                    = new BufferedReader(
+                    new InputStreamReader((clientSocket.getInputStream())));
+
+            while (true) {
+                var line = inputFromServer.readLine();
+                if (line.isEmpty()) {
+                    break;
+                }
+                System.out.println(line);
+            }
+            clientSocket.close();
+            inputFromServer.close();
             output.close();
 
         } catch (IOException e) {
--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,18 +9,30 @@ public class Main {                                                     // SERVE
 
     public static void main(String[] args) {
 
-        try (ServerSocket socket = new ServerSocket(5050)) {
+        try (ServerSocket serverSocket = new ServerSocket(5050)) {
 
             while (true) {
-                Socket client = socket.accept();
-                System.out.println(client.getInetAddress());
+                Socket clientSocket = serverSocket.accept();
+                System.out.println(clientSocket.getInetAddress());
 
-                var inputFromClient = new BufferedReader(new InputStreamReader((client.getInputStream())));
+                var inputFromClient
+                        = new BufferedReader(
+                        new InputStreamReader((clientSocket.getInputStream())));
 
-                inputFromClient.lines().forEach(System.out::println);
+                while (true) {
+                    var line = inputFromClient.readLine();
+                    if (line.isEmpty()) {
+                        break;
+                    }
+                    System.out.println(line);
+                }
 
+                var outputToClient = new PrintWriter(clientSocket.getOutputStream());
+                outputToClient.print("HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+                outputToClient.flush();
+                outputToClient.close();
                 inputFromClient.close();
-                client.close();
+                clientSocket.close();
             }
 //            String input = "";
 //            while (input != null) {
