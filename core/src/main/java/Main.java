d82cdbdf2a997f9fd12836754ee022e76c83f380
JoLiJava <73590210+JoLiJava@users.noreply.github.com>
2021-06-03 19:34:00 +0200

    Synchronized version of 'billboard' server implementation

--- core/src/main/java/Main.java
+++ core/src/main/java/Main.java
@@ -11,8 +11,7 @@ import java.util.concurrent.Executors;
 
 public class Main {                                                     //SERVER
 
-    //Skriv en server som sparar inkommande information
-    //och sen returnerar all sparad information som svar.
+
     public static List<String> billboard = new ArrayList<>();
 
 
@@ -23,9 +22,6 @@ public class Main {                                                     //SERVER
         try (ServerSocket serverSocket = new ServerSocket(5050)) {  //växel
             while (true) {
                 Socket clientSocket = serverSocket.accept();        //anslutning
-                //Starta tråd
-                //Thread thread = new Thread(() -> handleConnection(clientSocket));
-                //thread.start();
                 executorService.submit(() -> handleConnection(clientSocket));
             }
         } catch (IOException e) {
@@ -35,37 +31,51 @@ public class Main {                                                     //SERVER
 
     private static void handleConnection(Socket clientSocket) {
         try {
-            System.out.println(clientSocket.getInetAddress());
-            System.out.println(Thread.currentThread().getName());
-            Thread.sleep(500);
             var inputFromClient
                     = new BufferedReader(
                     new InputStreamReader((clientSocket.getInputStream())));
 
-            while (true) {
-                var line = inputFromClient.readLine();
-                if (line == null || line.isEmpty()) {
-                    break;
-                }
-                billboard.add(line);
-                System.out.println(line);
-            }
+            readRequest(inputFromClient);
 
             var outputToClient
                     = new PrintWriter(clientSocket.getOutputStream());
-            //outputToClient.print(
-                    //"HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+            sendResponse(outputToClient);
 
-            for (String line : billboard) {
-                outputToClient.print(line + "\r\n");
-            }
-            outputToClient.println("\r\n");
-            outputToClient.flush();
             outputToClient.close();
             inputFromClient.close();
             clientSocket.close();
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void sendResponse(PrintWriter outputToClient) {
+        //outputToClient.print(
+        //"HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n");
+
+        synchronized (billboard) {
+            for (String line : billboard) {
+                outputToClient.print(line + "\r\n");
+            }
+        }
+        outputToClient.println("\r\n");
+        outputToClient.flush();
+    }
+
+    private static void readRequest(BufferedReader inputFromClient) throws IOException {
+        List<String> tempList = new ArrayList<>();
+
+        while (true) {
+            var line = inputFromClient.readLine();
+            if (line == null || line.isEmpty()) {
+                break;
+            }
+            tempList.add(line);
+            System.out.println(line);
+        }
+
+        synchronized (billboard) {
+            billboard.addAll(tempList);
         }
     }
 }
